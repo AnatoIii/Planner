c3de699a02389494f4e8108c2f73297dc1153b0a
WeirdFish09 <kovtkoyura@gmail.com>
2021-03-02 22:56:22 +0200

    Added initial dao

---
+++ src/main/java/com/weirdsoft/Planner/dao/GenericDao.java
@@ -0,0 +1,10 @@
+package com.weirdsoft.Planner.dao;
+
+import java.util.UUID;
+
+public interface GenericDao<T> {
+    T find(UUID id);
+    T create(T object);
+    UUID delete(UUID id);
+    UUID update(T object);
+}
---
+++ src/main/java/com/weirdsoft/Planner/dao/impl/NoteDaoImpl.java
@@ -0,0 +1,75 @@
+package com.weirdsoft.Planner.dao.impl;
+
+import com.weirdsoft.Planner.dao.DaoUtils;
+import com.weirdsoft.Planner.dao.NoteDao;
+import com.weirdsoft.Planner.mappers.NoteMapper;
+import com.weirdsoft.Planner.models.Note;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class NoteDaoImpl implements NoteDao {
+    private static final String getByIdSql = "select noteId, name, description, dateTime, creatorId from Notes where noteId = ?";
+    private static final String deleteByIdSql = "delete from Notes where noteid = ? RETURNING noteid ";
+    private static final String createSql = "insert into Notes(noteId, name, description, dateTime, creatorId) VALUES (?, ?, ?, ?, ?) RETURNING noteId";
+    private static final String updateSql = "update notes set name=?, description=? WHERE noteid=? RETURNING noteid";
+
+    @Override
+    public Note find(UUID id) {
+        return DaoUtils.executeAndMap(getByIdSql, NoteMapper::mapNote, (ps) -> {
+            try {
+                ps.setString(1, id.toString());
+            } catch (Exception e) {
+
+            }
+        });
+    }
+
+    @Override
+    public Note create(Note object) {
+        UUID newId = UUID.randomUUID();
+        DaoUtils.executeAndMap(createSql,DaoUtils::uuidMapper,(ps) -> {
+            try {
+                ps.setString(1,newId.toString());
+                ps.setString(2,object.getName());
+                ps.setString(3,object.getDescription());
+                ps.setTimestamp(4,new Timestamp(object.getDateTime().getTime()));
+                String creatorId = null;
+                if(object.getCreatorId() != null){
+                    creatorId = object.getCreatorId().toString();
+                }
+                ps.setString(5,creatorId);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        });
+        object.setNoteId(newId);
+        return object;
+    }
+
+    @Override
+    public UUID delete(UUID id) {
+        return DaoUtils.executeAndMap(deleteByIdSql, DaoUtils::uuidMapper, (ps) -> {
+            try {
+                ps.setString(1,id.toString());
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        });
+    }
+
+    @Override
+    public UUID update(Note object) {
+        return DaoUtils.executeAndMap(updateSql, DaoUtils::uuidMapper, (ps) -> {
+            try {
+                ps.setString(1, object.getName());
+                ps.setString(2, object.getDescription());
+                ps.setString(3, object.getNoteId().toString());
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        });
+    }
+}
