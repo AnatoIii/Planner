c9ec205ebc117f30f018d68b55b67d5c41660e6f
Anatolii Ivanov <anatolii-ivanov@binary-studio.com>
2021-03-03 20:30:32 +0200

    Add create note form

--- src/main/java/com/weirdsoft/Planner/servlets/NoteCreateServlet.java
+++ src/main/java/com/weirdsoft/Planner/servlets/NoteCreateServlet.java
@@ -2,9 +2,12 @@ package com.weirdsoft.Planner.servlets;
 
 import com.weirdsoft.Planner.dao.UserDao;
 import com.weirdsoft.Planner.dao.impl.UserDaoImpl;
+import com.weirdsoft.Planner.models.dtos.CategoryTO;
 import com.weirdsoft.Planner.services.UserService;
 import com.weirdsoft.Planner.services.impl.UserServiceImpl;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.UUID;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
@@ -27,7 +30,30 @@ public class NoteCreateServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
         
+        ArrayList<CategoryTO> categories = new ArrayList<>();
+        categories.add(new CategoryTO("Category 1", UUID.randomUUID()));
+        categories.add(new CategoryTO("Category 2", UUID.randomUUID()));
+        categories.add(new CategoryTO("Category 3", UUID.randomUUID()));
+        
+        request.setAttribute("categories", categories);
+        
         RequestDispatcher view = request.getRequestDispatcher("Pages/Forms/createNote.jsp");
+        view.forward(request, response);
+    }
+    
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
+                
+        String name = request.getParameter("name");
+        String date = request.getParameter("date");
+        String time = request.getParameter("time");
+        UUID categoryId = UUID.fromString(request.getParameter("categoryId"));
+        String description = request.getParameter("description");
+        
+        String destPage = "Auth/Success.jsp";
+        
+        RequestDispatcher view = request.getRequestDispatcher(destPage);
         view.forward(request, response);
     }
 }
